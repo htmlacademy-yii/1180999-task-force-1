ec5df482cdc5d9f8f379a55aa005033945c90f4f
Rashid <mahraway@gmail.com>
2021-06-20 00:06:04 +0300

    Задания и исполнители

---
+++ frontend/controllers/TasksController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+use frontend\models\Tasks;
+
+class TasksController extends Controller
+{
+    /**
+     * Функция формирует объект с данными задач
+     * @return string
+     */
+    public function actionIndex(): string
+    {
+        $tasks = Tasks::find()->all();
+
+        return $this->render('tasks', [
+            'tasks' => $tasks
+        ]);
+    }
+
+}
