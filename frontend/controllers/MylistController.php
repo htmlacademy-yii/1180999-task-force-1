84ee2bedfe424d06306e2b0be92b791e59093bec
Rashid <mahraway@gmail.com>
2021-12-22 20:21:53 +0300

    Исправил вывод задач на странице - Мои задания

--- frontend/controllers/MylistController.php
+++ frontend/controllers/MylistController.php
@@ -19,7 +19,7 @@ class MylistController extends SecuredController
     public function actionIndex($status = null)
     {
         $user = Users::findOne(\Yii::$app->user->identity->getId());
-        $query = Tasks::find()->where(['user_id' => $user->id]);
+        $query = Tasks::find()->where(['user_id' => $user->id])->orWhere(['executor_id' => $user->id]);
 
         switch ($status) {
             case Task::STATUS_NEW_EN:
@@ -29,11 +29,11 @@ class MylistController extends SecuredController
                 $query->andWhere(['status' => Task::STATUS_IN_WORK]);
                 break;
             case Task::STATUS_CANCEL_EN:
-                $query->andWhere(['status' => Task::STATUS_CANCEL]);
+                $query->andWhere(['status' => [Task::STATUS_FAIL, Task::STATUS_CANCEL]]);
                 break;
             case Task::STATUS_SUCCESS_EN:
             case Task::STATUS_FAIL_EN:
-                $query->andWhere(['status' => [Task::STATUS_FAIL, Task::STATUS_SUCCESS]]);
+                $query->andWhere(['status' =>  Task::STATUS_SUCCESS]);
                 break;
             case Task::STATUS_HIDDEN_EN:
                 $query->andWhere(['status' => Task::STATUS_HIDDEN]);
